0ccca3981aa053ffca548a36fd968ca0d5b6a968
ClashofLights <clashoflightsdonate@gmail.com>
2016-09-15 19:17:08 +0200

    Update ConstructionItemData.cs

--- Ultrapowa Clash Server/Files/Logic/ConstructionItemData.cs
+++ Ultrapowa Clash Server/Files/Logic/ConstructionItemData.cs
@@ -1,4 +1,4 @@
-﻿/*
+/*
  * Program : Ultrapowa Clash Server
  * Description : A C# Writted 'Clash of Clans' Server Emulator !
  *
@@ -25,35 +25,17 @@ namespace UCS.Files.Logic
 
         #region Public Methods
 
-        public virtual int GetBuildCost(int level)
-        {
-            return -1;
-        }
+        public virtual int GetBuildCost(int level) => -1;
 
-        public virtual ResourceData GetBuildResource(int level)
-        {
-            return null;
-        }
+        public virtual ResourceData GetBuildResource(int level) => null;
 
-        public virtual int GetConstructionTime(int level)
-        {
-            return -1;
-        }
+        public virtual int GetConstructionTime(int level) => -1;
 
-        public virtual int GetRequiredTownHallLevel(int level)
-        {
-            return -1;
-        }
+        public virtual int GetRequiredTownHallLevel(int level) => -1;
 
-        public virtual int GetUpgradeLevelCount()
-        {
-            return -1;
-        }
+        public virtual int GetUpgradeLevelCount() => -1;
 
-        public virtual bool IsTownHall()
-        {
-            return false;
-        }
+        public virtual bool IsTownHall() => false;
 
         #endregion Public Methods
     }
