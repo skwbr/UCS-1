11f018ea656e3a76465e27713a957ce7eda606e5
ClashofLights <clashoflightsdonate@gmail.com>
2016-09-16 14:53:18 +0200

    Update TriggerComponent.cs

--- Ultrapowa Clash Server/Logic/Component/TriggerComponent.cs
+++ Ultrapowa Clash Server/Logic/Component/TriggerComponent.cs
@@ -1,4 +1,4 @@
-﻿/*
+/*
  * Program : Ultrapowa Clash Server
  * Description : A C# Writted 'Clash of Clans' Server Emulator !
  *
@@ -21,10 +21,7 @@ namespace UCS.Logic
 
         #region Public Properties
 
-        public override int Type
-        {
-            get { return 8; }
-        }
+        public override int Type => 8;
 
         #endregion Public Properties
     }
