b5b11d0658903e91fd566dadc359a036cbf4c6b6
ClashofLights <clashoflightsdonate@gmail.com>
2016-09-16 14:50:25 +0200

    Update BunkerComponent.cs

--- Ultrapowa Clash Server/Logic/Component/BunkerComponent.cs
+++ Ultrapowa Clash Server/Logic/Component/BunkerComponent.cs
@@ -1,4 +1,4 @@
-﻿/*
+/*
  * Program : Ultrapowa Clash Server
  * Description : A C# Writted 'Clash of Clans' Server Emulator !
  *
@@ -21,10 +21,7 @@ namespace UCS.Logic
 
         #region Public Properties
 
-        public override int Type
-        {
-            get { return 7; }
-        }
+        public override int Type => 7;
 
         #endregion Public Properties
     }
